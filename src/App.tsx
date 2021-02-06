d0cbcc9f88a33052df35477b5dc2dba3be44c4cb
Vít Herain <vit.herain@zonky.cz>
2021-02-06 13:12:51 +0100

    ConfigCat with polling and watching FF value

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ChangeEvent, useCallback, useEffect, useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Market from "./Market";
+import Wallet from "./Wallet";
+import Referral from "./Referral";
+import {AppBar, Button, Toolbar} from "@material-ui/core";
+import * as configcat from "configcat-js";
+
+function createConfigCatClient(configChanged: () => void) {
+  const logger = configcat.createConsoleLogger(3);
+  return configcat.createClientWithAutoPoll(
+    "GcrYCMnuoU2LWo79dWhcXA/DU8FyDeGXEyg58xtJPqf0g",
+    {
+      pollIntervalSeconds: 10,
+      logger: logger,
+      configChanged
+    }
+  );
+}
 
 function App() {
+  const [referralEnabled, setReferralEnabled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const configChanged = async () => {
+      console.log('notified about config change event, fetching FF');
+      const value = await configCatClient.getValueAsync("referralEnabled", false);
+      setReferralEnabled(value);
+    };
+
+    const configCatClient = createConfigCatClient(configChanged);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Button color="inherit"><Link to="/">Market</Link></Button>
+            <Button color="inherit"><Link to="/wallet">Wallet</Link></Button>
+            {referralEnabled && (<Button color="inherit"><Link to="/referral">Referral</Link></Button>)}
+          </Toolbar>
+        </AppBar>
+
+        <Switch>
+          <Route path="/wallet">
+            <Wallet/>
+          </Route>
+          <Route path="/referral">
+            <Referral/>
+          </Route>
+          <Route path="/">
+            <Market/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Wallet.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Wallet: React.FC = () => {
+    return (
+        <div>This is Wallet</div>
+    );
+};
+
+export default Wallet;
